598db303f8877bbfb06bd335273ee75f5fe407d5
redpandaathome <leeyumi415@gmail.com>
2022-02-18 18:32:29 +0900

    added test route "/"

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.listen('/', (req, res, next) => {
+   console.log(`Caliing '/'...`);
+   res.status(200).send('Hello World, 2022 🐯🐯🐯!')
+})
+
 app.listen(port, ()=>{
    console.log(`Server is on port no.${port}...👂`);
 }).on('error', (err)=>{

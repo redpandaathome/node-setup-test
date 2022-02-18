26ab3780348634a96b275f3b1158f0f477974257
redpandaathome <leeyumi415@gmail.com>
2022-02-18 19:28:27 +0900

    + NODE_ENV type

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const config = require("./config").config;
 
+if (process.env.NODE_ENV == 'production') {
+   console.log("Production Mode");
+} else if (process.env.NODE_ENV == 'development') {
+   console.log("Development Mode");
+   const morgan = require("morgan");
+   app.use(morgan('dev'));
+}
+
 const mysql = require('mysql');
 const connection = mysql.createConnection({
    host: config.db.host,
